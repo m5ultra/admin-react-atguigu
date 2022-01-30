855f5317910cb2084d43319f776cc11078689ae9
Dendi <352671309@qq.com>
2022-01-30 10:43:18 +0800

    axios 重新封装

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,53 +1,30 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 import * as qs from 'qs'
-import { base } from '../conf'
-const defaultOptions = {
-  // @ts-ignore
-  baseURL: base[process.env.NODE_ENV].apiBaseUrl,
-  timeout: 10000,
-  withCredentials: true,
+import {base} from "../conf"
+
+/**
+ * 1.初始化一个service
+ * 2.
+ */
+const service = axios.create({
+  baseURL: base[process.env.NODE_ENV].baseURL, // baseUrl e.g: http://localhost:8080/api/
   headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    token: 'this is token todo',
+    'X-Requested-With': 'XMLHttpRequest',
+    token: 'token...',
   },
-}
-axios.interceptors.request.use(
-  (config: any): any => {
-    Object.assign(config, defaultOptions)
-    console.log(config, 'axios.interceptors.request')
-    // 处理data数据
-    config.transformRequest = [
-      (data: any) => {
-        return qs.stringify(data, {
-          allowDots: true,
-        })
-      },
-    ]
-    // 处理query参数
-    config.paramsSerializer = (params: any) => {
-      return qs.stringify(params, {
-        arrayFormat: 'repeat',
-      })
+  withCredentials: true,  // 是否跨站点访问控制请求
+  timeout: 30000, // 超时时间
+  validateStatus() {  // TODO 待验证（使用async-await，处理reject情况较为繁琐，所以全部返回resolve，在业务代码中处理异常）
+    return true
+  },
+  transformRequest: [(data) => { // 处理data数据
+    data = JSON.stringify(data)
+    return data
+  }],
+  transformResponse: [(data) => {
+    if (typeof data === 'string' && data.startsWith('{')) { // 处理返回
+      data = JSON.parse(data)
     }
-    return config
-  },
-  (error: any) => {
-    return Promise.reject(error)
-  },
-)
-
-axios.interceptors.response.use(
-  (response: any): any => {
-    const { data } = response
-    console.log(data, 'axios.interceptors.response')
-    if (data.code === 2) {
-      window.location.href = ``
-    }
-    return response
-  },
-  (error: any) => {
-    return Promise.reject(error)
-  },
-)
-
-export default axios
+    return data
+  }]
+})
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,7 +2,7 @@ import './login.less'
 import logo from './images/logo.png'
 import { Form, Input, Button } from 'antd'
 import { UserOutlined, LockOutlined } from '@ant-design/icons'
-import axios from '../../api/http'
+// import axios from '../../api/http'
 import { useEffect } from 'react'
 /**
  用户名/密码的合法性要求
@@ -19,7 +19,7 @@ const Login = () => {
   const onFinish = async (values: {username: string, password: string}) => {
     console.log(values)
     const { username, password } = values
-    const result = await axios({ url: 'login', method: 'POST', data: { username, password } })
+    // const result = await axios({ url: 'login', method: 'POST', data: { username, password } })
   }
   return (
     <div className="login">
