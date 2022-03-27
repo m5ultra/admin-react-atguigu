6a5ca22a98fb4875cbfdb2ff2f282a1fecba29d0
Dendi <352671309@qq.com>
2022-03-27 16:10:42 +0800

    测试状态管理

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
 import Login from './pages/login/login'
 import Admin from './pages/admin/admin'
 import useLoader from './hooks/useLoading'
 import { Provider } from 'react-redux'
-import store from './store'
+import { store, persistor } from './store'
 export default () => {
   const [loader] = useLoader() //initialize useLoader hook
   return (
     <Provider store={store}>
-      {loader}
-      <Router>
-        <Routes>
-          <Route path={'/login'} element={<Login />} />
-          <Route path={'/*'} element={<Admin />} />
-        </Routes>
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        {loader}
+        <Router>
+          <Routes>
+            <Route path={'/login'} element={<Login />} />
+            <Route path={'/*'} element={<Admin />} />
+          </Routes>
+        </Router>
+      </PersistGate>
     </Provider>
   )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,15 @@
 import { createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import storageSession from 'redux-persist/lib/storage/session'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import reducer from './reducers'
+import rootReducer from './reducers'
+const persistConfig = {
+  key: 'test',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export const store = createStore(persistedReducer, composeWithDevTools())
+export const persistor = persistStore(store)
 
-const store = createStore(reducer, composeWithDevTools())
 
-export default store
