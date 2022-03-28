ec7b8511e39511040c1a4ae36b1a8b95a9931b20
Dendi <352671309@qq.com>
2022-03-28 22:35:15 +0800

    测试使用select获取state

--- src/store/redux-saga.ts
+++ src/store/redux-saga.ts
@@ -1,7 +1,13 @@
-import { takeEvery } from 'redux-saga/effects'
+import { takeEvery, select } from 'redux-saga/effects'
 import { constants } from '../pages/home/store'
 export function* rootSagas() {
   console.log('Hello Sagas!')
+  // @ts-ignore
+  const state = yield select( x => {
+    console.log(x)
+    return x
+  })
+  console.log(state, '~~~~state')
   yield takeEvery(constants.INCREMENT_NUM, () => {
     console.log('hello, takeEvery')
   })
